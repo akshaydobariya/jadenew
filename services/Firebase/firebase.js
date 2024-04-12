b164fc115307fd9da3f0a38c21998212f36e701b
Gaurav Panwar <gauravpanwar.websenor@gmail.com>
2024-04-12 20:23:31 +0530

    firebase issue

--- services/Firebase/firebase.js
+++ services/Firebase/firebase.js
@@ -1,4 +1,26 @@
 // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAbhycbnuDRAE94PyWwnz8YngrIpIdWhT8",
+//   authDomain: "jadescrolls-a9b16.firebaseapp.com",
+//   projectId: "jadescrolls-a9b16",
+//   storageBucket: "jadescrolls-a9b16.appspot.com",
+//   messagingSenderId: "727229574438",
+//   appId: "1:727229574438:web:08fa88ef2d4461634765e7",
+//   measurementId: "G-J40DV6JPYW"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -17,29 +39,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const firebaseApp = initializeApp(firebaseConfig);
 
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDt5nyPsqxPUdIuHOFVJixwB5RG5q-idOg",
-//   authDomain: "my-demo-5667d.firebaseapp.com",
-//   projectId: "my-demo-5667d",
-//   storageBucket: "my-demo-5667d.appspot.com",
-//   messagingSenderId: "802442928131",
-//   appId: "1:802442928131:web:996167f16845f984246e36",
-//   measurementId: "G-P1K66P6YJ1"
-// };
-
-// // Initialize Firebase
-// const firebaseApp = initializeApp(firebaseConfig);
-
-// export default firebaseApp;
-// //export const analytics = getAnalytics(app);
+export default firebaseApp;
+//export const analytics = getAnalytics(app);

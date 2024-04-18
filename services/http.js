1baa173c552a4d5f2751e684feb63b905862676d
Imroz <imroz.websenor@gmail.com>
2024-04-18 20:17:13 +0530

    change baseUrl

--- services/http.js
+++ services/http.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const http = axios.create({
-    baseURL: "https://api.jadescrolls.com/api/"
-    //baseURL: "https://zscroll.peclick.com/api/"
+    // baseURL: "https://api.jadescrolls.com/api/"
+    baseURL: "https://zscroll.peclick.com/api/"
 })

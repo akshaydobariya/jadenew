20014a350d637fbed5ab633c95c1be397c5119ab
Gaurav Panwar <gauravpanwar.websenor@gmail.com>
2024-05-31 10:52:46 +0530

    razorpay code merge

--- app/Home/page.js
+++ app/Home/page.js
@@ -58,7 +58,7 @@ async function HomePage() {
 
                 <PopularNovels />
 
-                <NovelByGenre  />
+                {/* <NovelByGenre  /> */}
 
                 <FeaturedBook  />
 

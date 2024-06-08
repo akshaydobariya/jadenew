ab5843549595eb36968b334bb1bd5debecddc485
imroz <imroz.websenor@gmail.com>
2024-06-08 17:36:17 +0530

    change url

--- appConfig.js
+++ appConfig.js
@@ -1,7 +1,7 @@
 const AppConfig = {
-  // apiUrl: "https://api.jadescrolls.com/api/"
+  apiUrl: "https://api.jadescrolls.com/api/",
 
-  apiUrl: "https://zscroll.peclick.com/api/",
+  // apiUrl: "https://zscroll.peclick.com/api/",
   // apiUrl: "http://localhost:4000/api/",
   currency: "₹",
 };

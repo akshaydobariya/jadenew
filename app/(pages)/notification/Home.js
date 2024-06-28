b3c4fc8c2bad5272287601c841da6d540ce255b9
imroz <imroz.websenor@gmail.com>
2024-06-28 14:53:00 +0530

    loader

--- app/(pages)/notification/Home.js
+++ app/(pages)/notification/Home.js
@@ -1,63 +1,88 @@
-'use client'
-import PaginationControlled from '@/components/pagination'
-import useApiService from '@/services/ApiService'
-import moment from 'moment'
-import React, { useEffect, useState } from 'react'
+"use client";
+import PaginationControlled from "@/components/pagination";
+import useApiService from "@/services/ApiService";
+import moment from "moment";
+import React, { useEffect, useState } from "react";
+import loader from "../../../public/assets/loader/loader.gif";
+import Image from "next/image";
 
 function Home() {
-    const { getAllNotificaiton } = useApiService()
-    const [notificationData, setNotificationData] = useState([])
-    const [page, setPage] = useState(1)
+  const { getAllNotificaiton } = useApiService();
+  const [notificationData, setNotificationData] = useState([]);
+  const [page, setPage] = useState(1);
 
-    useEffect(() => {
-        if(typeof window !== 'undefined'){
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        })}
-        const url = `page=${page}&limit=10`
-        getAllNotificaiton(url).then((res) => {
-            setNotificationData(res?.data?.data)
-        }).catch((er) => {
-        })
-    }, [page])
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+    const url = `page=${page}&limit=10`;
+    getAllNotificaiton(url)
+      .then((res) => {
+        setNotificationData(res?.data?.data);
+      })
+      .catch((er) => {});
+  }, [page]);
 
-    return (
-        <div className='w-[90%] md:w-[80%] lg:w-3/5 pt-4 xl:pt-14 xl:pb-6 flex mx-auto'>
-            <div className='w-full my-10 px-26 bg-white dark:bg-[#202020] p-10 h-[100vh] xl:h-full'>
-                <div className='flex items-center justify-center'>
-                    <div className='text-2xl font-semibold text-gray-800 dark:text-gray-200'>Notifications</div>
-                    {/* <div className='text-sm underline'>Mark all as read</div> */}
-                </div>
-                <hr className=' my-2 mb-8 bg-slate-700' />
-                {notificationData?.data?.length == 0 ?
-                    <div className='text-center'>No Notification Available</div> :
-                    notificationData?.data?.map((item, index) => {
-                        return (
-                            <div key={index} className='flex justify-between py-2 border-b-2 hover:bg-gray-200 dark:hover:bg-gray-700 px-2 text-gray-700 dark:text-gray-200'>
-                                <div>
-                                    <div>{item?.title}</div>
-                                    <div className=''>{item?.type}</div>
-                                </div>
-                                <div className='text-sm'>{moment(item?.createdAt).format('DD MMM, YYYY')}</div>
-                            </div>
-                        )
-                    })
-                }
-
-                {notificationData?.data?.length > 0 && (
-                    <div className='flex justify-center'>
-                        <PaginationControlled
-                            setPage={setPage}
-                            last_page={notificationData?.totalPage}
-                            page={page}
-                        />
-                    </div>
-                )}
+  return (
+    <div className="w-[90%] md:w-[80%] lg:w-3/5 pt-4 xl:pt-14 xl:pb-6 flex mx-auto">
+      <div className="w-full my-10 px-26 bg-white dark:bg-[#202020] p-10 h-[100vh] xl:h-full">
+        {notificationData?.data == undefined ? (
+          <div className="flex justify-center items-center w-full pt-16 pb-16 bg-white h-full">
+            <Image
+              src={loader}
+              alt=""
+              height={1000}
+              width={1000}
+              className="h-20 w-20"
+            />
+          </div>
+        ) : (
+          <>
+            <div className="flex items-center justify-center">
+              <div className="text-2xl font-semibold text-gray-800 dark:text-gray-200">
+                Notifications
+              </div>
+              {/* <div className='text-sm underline'>Mark all as read</div> */}
             </div>
+            <hr className=" my-2 mb-8 bg-slate-700" />
+            {notificationData?.data?.length == 0 ? (
+              <div className="text-center">No Notification Available</div>
+            ) : (
+              notificationData?.data?.map((item, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="flex justify-between py-2 border-b-2 hover:bg-gray-200 dark:hover:bg-gray-700 px-2 text-gray-700 dark:text-gray-200"
+                  >
+                    <div>
+                      <div>{item?.title}</div>
+                      <div className="">{item?.type}</div>
+                    </div>
+                    <div className="text-sm">
+                      {moment(item?.createdAt).format("DD MMM, YYYY")}
+                    </div>
+                  </div>
+                );
+              })
+            )}
+          </>
+        )}
 
-        </div>
-    )
+        {notificationData?.data?.length > 0 && (
+          <div className="flex justify-center">
+            <PaginationControlled
+              setPage={setPage}
+              last_page={notificationData?.totalPage}
+              page={page}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
--- app/(pages)/purchaseHistory/Home.js
+++ app/(pages)/purchaseHistory/Home.js
@@ -1,78 +1,127 @@
-'use client'
-import Image from 'next/image'
-import React, { useEffect, useState } from 'react'
-import { useRouter } from 'next/navigation';
-import useApiService from '@/services/ApiService';
-import PaginationControlled from '@/components/pagination';
-import Link from 'next/link';
+"use client";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import useApiService from "@/services/ApiService";
+import PaginationControlled from "@/components/pagination";
+import Link from "next/link";
+import loader from "../../../public/assets/loader/loader.gif";
 
 function Home() {
-    const router = useRouter()
-    const [transactionData, setTransactionData] = useState([])
-    const [page, setPage] = useState(1)
-    const { getTransaction } = useApiService()
+  const router = useRouter();
+  const [transactionData, setTransactionData] = useState([]);
+  const [page, setPage] = useState(1);
+  const { getTransaction } = useApiService();
 
-    useEffect(() => {
-        const url = `page=${page}&limit=10`
-        getTransaction(url).then((res) => {
-            setTransactionData(res?.data?.data)
-            if (typeof window !== 'undefined') {
-                window.scrollTo({
-                    top: 0,
-                    behavior: "smooth"
-                })
-            }
-        }).catch((er) => {
-            console.log(er);
-        })
-    }, [page])
+  useEffect(() => {
+    const url = `page=${page}&limit=10`;
+    getTransaction(url)
+      .then((res) => {
+        setTransactionData(res?.data?.data);
+        if (typeof window !== "undefined") {
+          window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+          });
+        }
+      })
+      .catch((er) => {
+        console.log(er);
+      });
+  }, [page]);
 
-    return (
-        <div className='pt-16'>
-            <div className='bg-gray-200 dark:bg-[#131415] lg:px-10 xl:px-52 px-2 md:px-8 pb-[11rem] h-[100vh] xl:h-full'>
-                <div className='text-center text-gray-800 pt-6 pb-3'>
-                    {transactionData?.transactions?.length > 0 && <div className='text-2xl dark:text-gray-200'>Purchase History</div>}
+  return (
+    <div className="pt-16">
+      <div className={`dark:bg-[#131415] lg:px-10 xl:px-52 px-2 md:px-8 pb-[11rem] h-[100vh] xl:h-full ${transactionData?.transactions == undefined ? "bg-white" : "bg-gray-200"}`}>
+        {transactionData?.transactions == undefined ? (
+          <div className="flex justify-center items-center bg-white h-full pt-20">
+            <Image
+              src={loader}
+              alt=""
+              height={1000}
+              width={1000}
+              className="h-20 w-20"
+            />
+          </div>
+        ) : (
+          <>
+            <div className="text-center text-gray-800 pt-6 pb-3">
+              {transactionData?.transactions?.length > 0 && (
+                <div className="text-2xl dark:text-gray-200">
+                  Purchase History
                 </div>
-                {transactionData?.transactions == 0 ?
-                    <div className='pt-40 md:pt-36 pb-48 text-center'>No Transactions Yet!</div>
-                    :
-                    transactionData?.transactions?.map((data, i) => {
-                        return (
-                            <Link href={{ pathname: `/detail/view/${data?.items[0]?.novelId?._id}` }} className='flex border-gray-400 rounded-md text-white shadow-md my-2 border bg-white dark:bg-gray-950'>
-                                <div>
-                                    <Image src={data?.items[0]?.novelId?.coverImg == null || data?.items[0]?.novelId?.coverImg == "null" ? "" : data?.items[0]?.novelId?.coverImg} height={300} width={300} alt={data?.items[0]?.name} className='h-[9rem] w-40 object-cover rounded-l-md' />
-                                </div>
-                                <div className='pl-3 flex justify-between w-full pr-2'>
-                                    <div>
-                                        <div className='text-lg text-gray-900 dark:text-gray-200'>{data?.items[0]?.name}</div>
-                                        <div className='flex text-sm list-disc gap-6 pt-1 text-gray-600 dark:text-gray-200'>
-                                            <div className='font-semibold'>{data?.items[0]?.fromChapter} - {data?.items[0]?.toChapter} chapter</div>
-                                        </div>
-                                        <div className='text-gray-600 dark:text-gray-200 text-sm'>
-                                            <div className='py-1'>Free Chapter +{data?.items[0]?.toChapter - data?.items[0]?.fromChapter + 1} Advance</div>
-                                            <div>Transaction id : {data?._id}</div>
-                                        </div>
-                                    </div>
-                                    <div className='hidden md:flex text-gray-600 dark:text-gray-200 pt-1 text-sm md:text-base flex-col justify-end md:justify-start pb-3'>
-                                        <div>Tier - {data?.items[0]?.tierName}</div>
-                                        <div>${data?.items[0]?.price} month</div>
-                                    </div>
-                                </div>
-                            </Link>
-                        )
-                    })
-                }
-
-                {transactionData?.transactions?.length > 0 && (
-                    <div className='flex justify-center'>
-                        <PaginationControlled
-                            setPage={setPage}
-                            last_page={transactionData?.totalPage}
-                            page={page}
-                        />
+              )}
+            </div>
+            {transactionData?.transactions == 0 ? (
+              <div className="pt-40 md:pt-36 pb-48 text-center">
+                No Transactions Yet!
+              </div>
+            ) : (
+              transactionData?.transactions?.map((data, i) => {
+                return (
+                  <Link
+                    href={{
+                      pathname: `/detail/view/${data?.items[0]?.novelId?._id}`,
+                    }}
+                    className="flex border-gray-400 rounded-md text-white shadow-md my-2 border bg-white dark:bg-gray-950"
+                  >
+                    <div>
+                      <Image
+                        src={
+                          data?.items[0]?.novelId?.coverImg == null ||
+                          data?.items[0]?.novelId?.coverImg == "null"
+                            ? ""
+                            : data?.items[0]?.novelId?.coverImg
+                        }
+                        height={300}
+                        width={300}
+                        alt={data?.items[0]?.name}
+                        className="h-[9rem] w-40 object-cover rounded-l-md"
+                      />
                     </div>
-                )}
-                {/* <div className='flex justify-between textThemeColor'>
+                    <div className="pl-3 flex justify-between w-full pr-2">
+                      <div>
+                        <div className="text-lg text-gray-900 dark:text-gray-200">
+                          {data?.items[0]?.name}
+                        </div>
+                        <div className="flex text-sm list-disc gap-6 pt-1 text-gray-600 dark:text-gray-200">
+                          <div className="font-semibold">
+                            {data?.items[0]?.fromChapter} -{" "}
+                            {data?.items[0]?.toChapter} chapter
+                          </div>
+                        </div>
+                        <div className="text-gray-600 dark:text-gray-200 text-sm">
+                          <div className="py-1">
+                            Free Chapter +
+                            {data?.items[0]?.toChapter -
+                              data?.items[0]?.fromChapter +
+                              1}{" "}
+                            Advance
+                          </div>
+                          <div>Transaction id : {data?._id}</div>
+                        </div>
+                      </div>
+                      <div className="hidden md:flex text-gray-600 dark:text-gray-200 pt-1 text-sm md:text-base flex-col justify-end md:justify-start pb-3">
+                        <div>Tier - {data?.items[0]?.tierName}</div>
+                        <div>${data?.items[0]?.price} month</div>
+                      </div>
+                    </div>
+                  </Link>
+                );
+              })
+            )}
+          </>
+        )}
+        {transactionData?.transactions?.length > 0 && (
+          <div className="flex justify-center">
+            <PaginationControlled
+              setPage={setPage}
+              last_page={transactionData?.totalPage}
+              page={page}
+            />
+          </div>
+        )}
+        {/* <div className='flex justify-between textThemeColor'>
                     <button className='flex items-center'>
                         <KeyboardBackspaceIcon fontSize='small' />
                         <div className='pl-1'>Preview</div>
@@ -82,9 +131,9 @@ function Home() {
                         <EastIcon fontSize='small' />
                     </button>
                 </div> */}
-            </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
